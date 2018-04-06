9b3ef3d2bc0005e57f6f44389f625ff127971e28
BigEgg <cd_bigegg@hotmail.com>
2018-04-05 19:30:10 -0700

    Lab5: cleanup code

--- lab5/goto_ball.py
+++ lab5/goto_ball.py
@@ -27,6 +27,8 @@ class BatteryAnnotator(cozmo.annotate.Annotator):
         text.render(d, bounds)
 
 # Define a decorator as a subclass of Annotator; displays the ball
+
+
 class BallAnnotator(cozmo.annotate.Annotator):
 
     ball = None
@@ -37,13 +39,14 @@ class BallAnnotator(cozmo.annotate.Annotator):
 
         if BallAnnotator.ball is not None:
 
-            #double size of bounding box to match size of rendered image
-            BallAnnotator.ball = np.multiply(BallAnnotator.ball,2)
+            # double size of bounding box to match size of rendered image
+            BallAnnotator.ball = np.multiply(BallAnnotator.ball, 2)
 
-            #define and display bounding box with params:
+            # define and display bounding box with params:
             #msg.img_topLeft_x, msg.img_topLeft_y, msg.img_width, msg.img_height
             box = cozmo.util.ImageBox(BallAnnotator.ball[0]-BallAnnotator.ball[2],
-                                      BallAnnotator.ball[1]-BallAnnotator.ball[2],
+                                      BallAnnotator.ball[1] -
+                                      BallAnnotator.ball[2],
                                       BallAnnotator.ball[2]*2, BallAnnotator.ball[2]*2)
             cozmo.annotate.add_img_box_to_image(image, box, "green", text=None)
 
@@ -53,28 +56,27 @@ class BallAnnotator(cozmo.annotate.Annotator):
 async def run(robot: cozmo.robot.Robot):
     '''The run method runs once the Cozmo SDK is connected.'''
 
-    #add annotators for battery level and ball bounding box
+    # add annotators for battery level and ball bounding box
     robot.world.image_annotator.add_annotator('battery', BatteryAnnotator)
     robot.world.image_annotator.add_annotator('ball', BallAnnotator)
-
 
     try:
 
         while True:
-            #get camera image
+            # get camera image
             event = await robot.world.wait_for(cozmo.camera.EvtNewRawCameraImage, timeout=30)
 
-            #convert camera image to opencv format
-            opencv_image = cv2.cvtColor(np.asarray(event.image), cv2.COLOR_RGB2GRAY)
+            # convert camera image to opencv format
+            opencv_image = cv2.cvtColor(
+                np.asarray(event.image), cv2.COLOR_RGB2GRAY)
 
-            #find the ball
+            # find the ball
             ball = find_ball.find_ball(opencv_image)
 
-            #set annotator ball
+            # set annotator ball
             BallAnnotator.ball = ball
 
-            ## TODO: ENTER YOUR SOLUTION HERE
-
+            # TODO: ENTER YOUR SOLUTION HERE
 
     except KeyboardInterrupt:
         print("")
@@ -83,7 +85,5 @@ async def run(robot: cozmo.robot.Robot):
         print(e)
 
 
-
 if __name__ == '__main__':
-    cozmo.run_program(run, use_viewer = True, force_viewer_on_top = True)
-
+    cozmo.run_program(run, use_viewer=True, force_viewer_on_top=True)

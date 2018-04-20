2610722cc3fa1632a3a664e247439731a9978100
BigEgg <cd_bigegg@hotmail.com>
2018-04-19 21:28:32 -0700

    Format code of Lab6 and Lab7

--- lab7/move_relative_to_cube.py
+++ lab7/move_relative_to_cube.py
@@ -15,32 +15,34 @@ from odometry import cozmo_go_to_pose
 sys.path.insert(0, '../lab6')
 from pose_transform import get_relative_pose
 
+
 def move_relative_to_cube(robot: cozmo.robot.Robot):
-	'''Looks for a cube while sitting still, when a cube is detected it 
-	moves the robot to a given pose relative to the detected cube pose.'''
+    '''Looks for a cube while sitting still, when a cube is detected it
+    moves the robot to a given pose relative to the detected cube pose.'''
 
-	robot.move_lift(-3)
-	robot.set_head_angle(degrees(0)).wait_for_completed()
-	cube = None
+    robot.move_lift(-3)
+    robot.set_head_angle(degrees(0)).wait_for_completed()
+    cube = None
 
-	while cube is None:
-		try:
-			cube = robot.world.wait_for_observed_light_cube(timeout=30)
-			if cube:
-				print("Found a cube, pose in the robot coordinate frame: %s" % get_relative_pose(cube.pose, robot.pose))
-		except asyncio.TimeoutError:
-			print("Didn't find a cube")
+    while cube is None:
+        try:
+            cube = robot.world.wait_for_observed_light_cube(timeout=30)
+            if cube:
+                print("Found a cube, pose in the robot coordinate frame: %s" %
+                      get_relative_pose(cube.pose, robot.pose))
+        except asyncio.TimeoutError:
+            print("Didn't find a cube")
 
-	desired_pose_relative_to_cube = Pose(0, 100, 0, angle_z=degrees(90))
+    desired_pose_relative_to_cube = Pose(0, 100, 0, angle_z=degrees(90))
 
-	# ####
-	# TODO: Make the robot move to the given desired_pose_relative_to_cube.
-	# Use the get_relative_pose function your implemented to determine the
-	# desired robot pose relative to the robot's current pose and then use
-	# one of the go_to_pose functions you implemented in Lab 6.
-	# ####
+    # ####
+    # TODO: Make the robot move to the given desired_pose_relative_to_cube.
+    # Use the get_relative_pose function your implemented to determine the
+    # desired robot pose relative to the robot's current pose and then use
+    # one of the go_to_pose functions you implemented in Lab 6.
+    # ####
 
 
 if __name__ == '__main__':
 
-	cozmo.run_program(move_relative_to_cube)
+    cozmo.run_program(move_relative_to_cube)
--- lab7/odometry.py
+++ lab7/odometry.py
@@ -12,157 +12,170 @@ import time
 
 # Wrappers for existing Cozmo navigation functions
 
+
 def cozmo_drive_straight(robot, dist, speed):
-	"""Drives the robot straight.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		dist -- Desired distance of the movement in millimeters
-		speed -- Desired speed of the movement in millimeters per second
-	"""
-	robot.drive_straight(distance_mm(dist), speed_mmps(speed)).wait_for_completed()
+    """Drives the robot straight.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            dist -- Desired distance of the movement in millimeters
+            speed -- Desired speed of the movement in millimeters per second
+    """
+    robot.drive_straight(distance_mm(
+        dist), speed_mmps(speed)).wait_for_completed()
+
 
 def cozmo_turn_in_place(robot, angle, speed):
-	"""Rotates the robot in place.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		angle -- Desired distance of the movement in degrees
-		speed -- Desired speed of the movement in degrees per second
-	"""
-	robot.turn_in_place(degrees(angle), speed=degrees(speed)).wait_for_completed()
+    """Rotates the robot in place.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            angle -- Desired distance of the movement in degrees
+            speed -- Desired speed of the movement in degrees per second
+    """
+    robot.turn_in_place(degrees(angle), speed=degrees(
+        speed)).wait_for_completed()
+
 
 def cozmo_go_to_pose(robot, x, y, angle_z):
-	"""Moves the robot to a pose relative to its current pose.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		x,y -- Desired position of the robot in millimeters
-		angle_z -- Desired rotation of the robot around the vertical axis in degrees
-	"""
-	robot.go_to_pose(Pose(x, y, 0, angle_z=degrees(angle_z)), relative_to_robot=True).wait_for_completed()
+    """Moves the robot to a pose relative to its current pose.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            x,y -- Desired position of the robot in millimeters
+            angle_z -- Desired rotation of the robot around the vertical axis in degrees
+    """
+    robot.go_to_pose(Pose(x, y, 0, angle_z=degrees(angle_z)),
+                     relative_to_robot=True).wait_for_completed()
 
 # Functions to be defined as part of the labs
 
+
 def get_front_wheel_radius():
-	"""Returns the radius of the Cozmo robot's front wheel in millimeters."""
-	# ####
-	# TODO: Empirically determine the radius of the robot's front wheel using the
-	# cozmo_drive_straight() function. You can write a separate script for doing 
-	# experiments to determine the radius. This function should return the radius
-	# in millimeters. Write a comment that explains how you determined it and any
-	# computation you do as part of this function.
-	# ####
-	pass
+    """Returns the radius of the Cozmo robot's front wheel in millimeters."""
+    # ####
+    # TODO: Empirically determine the radius of the robot's front wheel using the
+    # cozmo_drive_straight() function. You can write a separate script for doing
+    # experiments to determine the radius. This function should return the radius
+    # in millimeters. Write a comment that explains how you determined it and any
+    # computation you do as part of this function.
+    # ####
+    pass
+
 
 def get_distance_between_wheels():
-	"""Returns the distance between the wheels of the Cozmo robot in millimeters."""
-	# ####
-	# TODO: Empirically determine the distance between the wheels of the robot using
-	# robot.drive_wheels() function. Write a comment that explains how you determined
-	# it and any computation you do as part of this function.
-	# ####
-	pass
+    """Returns the distance between the wheels of the Cozmo robot in millimeters."""
+    # ####
+    # TODO: Empirically determine the distance between the wheels of the robot using
+    # robot.drive_wheels() function. Write a comment that explains how you determined
+    # it and any computation you do as part of this function.
+    # ####
+    pass
+
 
 def rotate_front_wheel(robot, angle_deg):
-	"""Rotates the front wheel of the robot by a desired angle.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		angle_deg -- Desired rotation of the wheel in degrees
-	"""
-	# ####
-	# TODO: Implement this function.
-	# ####
+    """Rotates the front wheel of the robot by a desired angle.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            angle_deg -- Desired rotation of the wheel in degrees
+    """
+    # ####
+    # TODO: Implement this function.
+    # ####
+
 
 def my_drive_straight(robot, dist, speed):
-	"""Drives the robot straight.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		dist -- Desired distance of the movement in millimeters
-		speed -- Desired speed of the movement in millimeters per second
-	"""
-	# ####
-	# TODO: Implement your version of a driving straight function using the
-	# robot.drive_wheels() function.
-	# ####
-	pass
+    """Drives the robot straight.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            dist -- Desired distance of the movement in millimeters
+            speed -- Desired speed of the movement in millimeters per second
+    """
+    # ####
+    # TODO: Implement your version of a driving straight function using the
+    # robot.drive_wheels() function.
+    # ####
+    pass
+
 
 def my_turn_in_place(robot, angle, speed):
-	"""Rotates the robot in place.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		angle -- Desired distance of the movement in degrees
-		speed -- Desired speed of the movement in degrees per second
-	"""
-	# ####
-	# TODO: Implement your version of a rotating in place function using the
-	# robot.drive_wheels() function.
-	# ####
-	pass
+    """Rotates the robot in place.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            angle -- Desired distance of the movement in degrees
+            speed -- Desired speed of the movement in degrees per second
+    """
+    # ####
+    # TODO: Implement your version of a rotating in place function using the
+    # robot.drive_wheels() function.
+    # ####
+    pass
+
 
 def my_go_to_pose1(robot, x, y, angle_z):
-	"""Moves the robot to a pose relative to its current pose.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		x,y -- Desired position of the robot in millimeters
-		angle_z -- Desired rotation of the robot around the vertical axis in degrees
-	"""
-	# ####
-	# TODO: Implement a function that makes the robot move to a desired pose
-	# using the my_drive_straight and my_turn_in_place functions. This should
-	# include a sequence of turning in place, moving straight, and then turning
-	# again at the target to get to the desired rotation (Approach 1).
-	# ####
-	pass
+    """Moves the robot to a pose relative to its current pose.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            x,y -- Desired position of the robot in millimeters
+            angle_z -- Desired rotation of the robot around the vertical axis in degrees
+    """
+    # ####
+    # TODO: Implement a function that makes the robot move to a desired pose
+    # using the my_drive_straight and my_turn_in_place functions. This should
+    # include a sequence of turning in place, moving straight, and then turning
+    # again at the target to get to the desired rotation (Approach 1).
+    # ####
+    pass
+
 
 def my_go_to_pose2(robot, x, y, angle_z):
-	"""Moves the robot to a pose relative to its current pose.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		x,y -- Desired position of the robot in millimeters
-		angle_z -- Desired rotation of the robot around the vertical axis in degrees
-	"""
-	# ####
-	# TODO: Implement a function that makes the robot move to a desired pose
-	# using the robot.drive_wheels() function to jointly move and rotate the 
-	# robot to reduce distance between current and desired pose (Approach 2).
-	# ####
-	pass
+    """Moves the robot to a pose relative to its current pose.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            x,y -- Desired position of the robot in millimeters
+            angle_z -- Desired rotation of the robot around the vertical axis in degrees
+    """
+    # ####
+    # TODO: Implement a function that makes the robot move to a desired pose
+    # using the robot.drive_wheels() function to jointly move and rotate the
+    # robot to reduce distance between current and desired pose (Approach 2).
+    # ####
+    pass
+
 
 def my_go_to_pose3(robot, x, y, angle_z):
-	"""Moves the robot to a pose relative to its current pose.
-		Arguments:
-		robot -- the Cozmo robot instance passed to the function
-		x,y -- Desired position of the robot in millimeters
-		angle_z -- Desired rotation of the robot around the vertical axis in degrees
-	"""
-	# ####
-	# TODO: Implement a function that makes the robot move to a desired pose
-	# as fast as possible. You can experiment with the built-in Cozmo function
-	# (cozmo_go_to_pose() above) to understand its strategy and do the same.
-	# ####
-	pass
+    """Moves the robot to a pose relative to its current pose.
+            Arguments:
+            robot -- the Cozmo robot instance passed to the function
+            x,y -- Desired position of the robot in millimeters
+            angle_z -- Desired rotation of the robot around the vertical axis in degrees
+    """
+    # ####
+    # TODO: Implement a function that makes the robot move to a desired pose
+    # as fast as possible. You can experiment with the built-in Cozmo function
+    # (cozmo_go_to_pose() above) to understand its strategy and do the same.
+    # ####
+    pass
+
 
 def run(robot: cozmo.robot.Robot):
 
-	print("***** Front wheel radius: " + str(get_front_wheel_radius()))
-	print("***** Distance between wheels: " + str(get_distance_between_wheels()))
+    print("***** Front wheel radius: " + str(get_front_wheel_radius()))
+    print("***** Distance between wheels: " +
+          str(get_distance_between_wheels()))
 
-	## Example tests of the functions
+    # Example tests of the functions
 
-	cozmo_drive_straight(robot, 62, 50)
-	cozmo_turn_in_place(robot, 60, 30)
-	cozmo_go_to_pose(robot, 100, 100, 45)
+    cozmo_drive_straight(robot, 62, 50)
+    cozmo_turn_in_place(robot, 60, 30)
+    cozmo_go_to_pose(robot, 100, 100, 45)
 
-	rotate_front_wheel(robot, 90)
-	my_drive_straight(robot, 62, 50)
-	my_turn_in_place(robot, 90, 30)
+    rotate_front_wheel(robot, 90)
+    my_drive_straight(robot, 62, 50)
+    my_turn_in_place(robot, 90, 30)
 
-	my_go_to_pose1(robot, 100, 100, 45)
-	my_go_to_pose2(robot, 100, 100, 45)
-	my_go_to_pose3(robot, 100, 100, 45)
+    my_go_to_pose1(robot, 100, 100, 45)
+    my_go_to_pose2(robot, 100, 100, 45)
+    my_go_to_pose3(robot, 100, 100, 45)
 
 
 if __name__ == '__main__':
 
-	cozmo.run_program(run)
-
-
-
+    cozmo.run_program(run)

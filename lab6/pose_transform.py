d475c79dde329e598dbf9fe53c03d0a231dacf18
BigEgg <cd_bigegg@hotmail.com>
2018-04-20 00:50:30 -0700

    Lab6: Add the Homogeneous Transforms way to get the relative position

--- lab6/pose_transform.py
+++ lab6/pose_transform.py
@@ -14,6 +14,38 @@ from cozmo.util import degrees
 
 
 def get_relative_pose(object_pose, reference_frame_pose):
+    # HOMOGENEOUS TRANSFORMS
+    # | cos(robot_angle), -sin(robot_angle), robot_x | | related_x |   | cube_x |
+    # | sin(robot_angle),  cos(robot_angle), robot_y | | related_y | = | cube_y |
+    # |                0,                 0,       1 | |         1 |   |      1 |
+    #
+    # ==>
+    #
+    # cos(robot_angle) * related_x - sin(robot_angle) * related_y + robot_x = cube_x
+    # sin(robot_angle) * related_x + cos(robot_angle) * related_y + robot_y = cube_y
+    #
+    # ==>
+    #
+    # related_x = cos(robot_angle) * (cube_x - robot_x) + sin(robot_angle) * (cube_y - robot_y)
+    # related_y = cos(robot_angle) * (cube_y - robot_y) - sin(robot_angle) * (cube_x - robot_x)
+    cube_x = object_pose.position.x
+    cube_y = object_pose.position.y
+    cube_angle_z = object_pose.rotation.angle_z
+
+    robot_x = reference_frame_pose.position.x
+    robot_y = reference_frame_pose.position.y
+    robot_angle = reference_frame_pose.rotation.angle_z
+
+    newX = math.cos(robot_angle.radians) * (cube_x - robot_x) + \
+        math.sin(robot_angle.radians) * (cube_y - robot_y)
+    newY = math.cos(robot_angle.radians) * (cube_y - robot_y) - \
+        math.sin(robot_angle.radians) * (cube_x - robot_x)
+    newAngle = cube_angle_z - robot_angle
+
+    return cozmo.util.pose_z_angle(newX, newY, 0, angle_z=newAngle, origin_id=object_pose._origin_id)
+
+
+def get_relative_pose2(object_pose, reference_frame_pose):
     # Point R as Robot, Point C as Cube, x as world X axis, x' as robot X axis
     # tan(RCx) = (cube_y - robot_y) / (cube_x - robot_x)
     # theta = RCx = atan((cube_y - robot_y) / (cube_x - robot_x))
@@ -65,6 +97,8 @@ def find_relative_cube_pose(robot: cozmo.robot.Robot):
                 print(f"Cube pose: {cube.pose}")
                 print(
                     f"Cube pose in the robot coordinate frame: {get_relative_pose(cube.pose, robot.pose)}")
+                print(
+                    f"Cube pose in the robot coordinate frame: {get_relative_pose2(cube.pose, robot.pose)}")
         except asyncio.TimeoutError:
             print("Didn't find a cube")
         time.sleep(10)
